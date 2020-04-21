fd3c6e64815627954d560b391f81de0d8b8ed3b5
sayashraaj <sayashraaj@gmail.com>
2020-04-21 05:53:50 +0530

    phone_validation

--- practical-react/src/components/Form.js
+++ practical-react/src/components/Form.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import '../App.css';
 
+const initialState = {			//reset to initial
+	name: "",
+	rollno: "",
+	phone: "",
+	email: "",
+	password: "",
+	confirmpassword: ""
+}
 export default class Form extends React.Component {
 
-	state={
-		name: "",
-		rollno: "",
-		phone: "",
-		email: "",
-		password: "",
-		confirmpassword: ""
-	};
+	state=initialState;
+
+
 	handleChangename=event =>{
 		console.log(event.target.value);
 		this.setState({name: event.target.value});
@@ -42,17 +45,26 @@ export default class Form extends React.Component {
 	};
 
 	validate=()=>{
-		let phoneError="";
+		let phoneError="";					//empty errors initialised
 		let emailError="";
 		let confirmpasswordError="";
+
+		if(this.state.phone.length!==10) {
+			phoneError="10 digit";
+		}
+		if(phoneError) {
+			this.setState({phoneError});
+			return false;
 
 		if(!this.state.email.includes('@smail.iitm.ac.in')) {
 			emailError="invalid smail";
 		}
-
 		if(emailError) {
-			this.setState({emailError});
+			this.setState({emailError});			//sets emailerror to invalid smail, else empty
 			return false;
+		}
+
+		
 		}
 
 		return true;
@@ -63,6 +75,8 @@ export default class Form extends React.Component {
 		const isValid=this.validate();
 		if(isValid){
 			console.log(this.state);
+			//clear form
+			this.setState(initialState);
 		}
 	};
 
@@ -73,6 +87,7 @@ export default class Form extends React.Component {
 			<div><input name="name" placeholder="name" value={this.state.name} onChange={this.handleChangename} /></div>
 			<div><input name="rollno" placeholder="roll number" value={this.state.rollno} onChange={this.handleChangerollno} /></div>
 			<div><input name="phone" placeholder="phone no" value={this.state.phone} onChange={this.handleChangephone} /></div>
+			<div style={{fontSize: 12, color: "red"}}> {this.state.phoneError}</div>
 			<div><input name="email" placeholder="email id" value={this.state.email} onChange={this.handleChangeemail} /></div>
 			<div style={{fontSize: 12, color: "red"}}> {this.state.emailError}</div>
 			<div><input name="password" placeholder="password" type="password" value={this.state.password} onChange={this.handleChangepassword} /></div>
